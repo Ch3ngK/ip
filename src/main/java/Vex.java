ff6edcd1a1a6041f0aee75c4b0543e1f89becb86
Ch3ngK <cheng.kw0123@gmail.com>
2026-01-22 18:53:55 +0800

    Added support for echo, and response for bye

--- src/main/java/Vex.java
+++ src/main/java/Vex.java
@@ -1,8 +1,26 @@
+import java.util.*;
 public class Vex {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String name = "Vex";
+
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
+        sc.close();
     }
 }
