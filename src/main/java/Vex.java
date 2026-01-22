ae900ece9b4c2dad7755798d0d94cd521d9c86da
Ch3ngK <cheng.kw0123@gmail.com>
2026-01-22 21:07:10 +0800

    Added Task class for use, and the function to mark tasks as done and undone

--- src/main/java/Vex.java
+++ src/main/java/Vex.java
@@ -3,8 +3,7 @@ public class Vex {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = "Vex";
-        String memory = "";
-        int number = 1;
+        ArrayList<Task> arr = new ArrayList<>(); //create a new arraylist storing the tasks completed
 
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
@@ -18,20 +17,79 @@ public class Vex {
                 System.out.println("____________________________________________________________");
                 break;
             }
+
             if (input.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(memory);
+                System.out.println("Here are the tasks in your list: ");
+                for (int i = 0; i < arr.size(); i++) {
+                    System.out.println((i + 1) + "." + arr.get(i).getStatus());
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            }
+
+            if (input.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                Task t = arr.get(taskNumber - 1);
+                t.markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(t.getStatus());
+                System.out.println("____________________________________________________________");
+                continue;
+            }
+            if (input.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                Task t = arr.get(taskNumber - 1);
+                t.markAsUndone();
+                System.out.println("____________________________________________________________");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(t.getStatus());
                 System.out.println("____________________________________________________________");
                 continue;
             }
 
             System.out.println("____________________________________________________________");
+            Task newTask = new Task(input);
+            arr.add(newTask);
             System.out.println("added: " + input);
             System.out.println("____________________________________________________________");
-            memory += number + ". " + input + "\n";
-            number++;
 
         }
         sc.close();
+    }
+}
+
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void complete() {
+        this.isDone = true;
+    }
+
+    public void incomplete() {
+        this.isDone = false;
+    }
+
+    public String getStatus() {
+        return "[" + getStatusIcon() + "] " + this.description;
+    }
+
+    public void markAsDone() {
+        this.complete();
+    }
+
+    public void markAsUndone() {
+        this.incomplete();
     }
 }
