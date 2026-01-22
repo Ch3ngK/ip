9a36baea6632e0ef6b36b742ac4b98aefbd0590a
Ch3ngK <cheng.kw0123@gmail.com>
2026-01-22 19:25:49 +0800

    Added ability to store text and display upon list input

--- src/main/java/Vex.java
+++ src/main/java/Vex.java
@@ -3,6 +3,8 @@ public class Vex {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = "Vex";
+        String memory = "";
+        int number = 1;
 
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
@@ -16,10 +18,19 @@ public class Vex {
                 System.out.println("____________________________________________________________");
                 break;
             }
+            if (input.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(memory);
+                System.out.println("____________________________________________________________");
+                continue;
+            }
 
             System.out.println("____________________________________________________________");
-            System.out.println(input);
+            System.out.println("added: " + input);
             System.out.println("____________________________________________________________");
+            memory += number + ". " + input + "\n";
+            number++;
+
         }
         sc.close();
     }
